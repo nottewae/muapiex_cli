4ccc92e218d58fb001013fe6daa31df7ef393b46
EvgeniyGorbunov <89217826141@mail.ru>
2021-10-14 00:23:16 +0300

    test

--- lib/client/client.ex
+++ lib/client/client.ex
@@ -104,7 +104,7 @@ defmodule MuapiExCli.Client do
     data = Poison.decode!(data)
     # IO.inspect data
     data = %{sign: sign, public_key: config()[:public_key], data: data, meta: meta}
-    # data |> IO.inspect
+    data |> IO.inspect
     # IO.inspect Poison.encode!(data)
     data = MuapiExCli.Helpers.Map.keys_to_atom(data)
     data = Poison.encode!(data)
